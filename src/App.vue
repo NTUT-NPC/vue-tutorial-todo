5508d47777492d627994f0c5165c664cea6a73b6
DevilTea <ch870814@gmail.com>
2019-05-29 15:50:34 +0800

    Finish layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,200 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <span class="header__title">Todooooooo</span>
+      <input class="header__add-btn" type="button" value="新增">
+    </div>
+    <div class="todo-list">
+      <div class="todo-list__item">
+        <input class="todo-item__checkbox" id="cb-1" type="checkbox">
+        <label for="cb-1"></label>
+        <span class="todo-item__text">waassssss</span>
+        <input class="todo-item__delete-btn" type="button" value="刪除">
+      </div>
+    </div>
+    <div class="add-todo-dialog">
+      <div class="add-todo-dialog__card">
+        <input class="add-todo-dialog__input" type="text" placeholder="請輸入待辦事項">
+        <div class="dialog-buttons">
+          <input class="dialog-buttons__confirm-btn" type="button" value="確認">
+          <input class="dialog-buttons__cancel-btn" type="button" value="取消">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style lang="scss">
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: 1;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Helvetica, Arial, "文泉驛正黑", "WenQuanYi Zen Hei", "黑體-繁",
+    "Heiti TC", "微軟正黑體", "Microsoft JhengHei", sans-serif;
+}
+
+.header {
+  height: 120px;
+  background: #2196f3;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 24px;
+}
+
+.header__title {
+  display: inline-block;
+  font-size: 10vmin;
+  font-size: 900;
+}
+
+.header__add-btn {
+  margin: auto 0 auto auto;
+  padding: 8px 16px;
+  display: inline-block;
+  border: 2px solid #ffffff;
+  border-radius: 20px;
+  background: transparent;
+  outline: none;
+  color: #ffffff;
+  font-size: 1.25rem;
+  cursor: pointer;
+}
+
+input[type="button"]:active {
+  transform: scale(0.95);
+}
+
+.todo-list {
+  max-width: 700px;
+  padding: 16px;
+  margin: auto;
+}
+
+.todo-list__item {
+  height: 70px;
+  margin: 8px 0;
+  padding: 16px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 4px #aaaaaa;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.todo-item__checkbox {
+  display: none;
+}
+
+.todo-item__checkbox + label {
+  visibility: hidden;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background: transparent;
+  border: 2px solid #777777;
+  border-radius: 5px;
+}
+
+.todo-item__checkbox:checked + label {
+  background: #4baeff;
+}
+
+.todo-item__checkbox:checked ~ .todo-item__text {
+  text-decoration: line-through;
+}
+
+.todo-item__text {
+  display: inline-block;
+  width: calc(100% - 180px);
+  font-size: 1.5rem;
+  color: #555555;
+}
+
+.todo-item__delete-btn {
+  visibility: hidden;
+  padding: 8px 16px;
+  display: inline-block;
+  border: 1px solid #ff3d3d;
+  border-radius: 20px;
+  background: transparent;
+  outline: none;
+  color: #ff3d3d;
+  cursor: pointer;
+}
+
+.todo-list__item:hover .todo-item__delete-btn,
+.todo-list__item:hover .todo-item__checkbox + label {
+  visibility: visible;
+}
+
+.add-todo-dialog {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.25);
+}
+
+.add-todo-dialog__card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  padding: 16px;
+  background: #ffffff;
+  border-radius: 5px;
+}
+
+.add-todo-dialog__input {
+  display: inline-block;
+  width: 100%;
+  height: 40px;
+  outline: none;
+  padding: 8px;
+  font-size: 18px;
+}
+
+.dialog-buttons {
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
+
+.dialog-buttons__cancel-btn {
+  padding: 8px 16px;
+  display: inline-block;
+  border: 1px solid #ff3d3d;
+  border-radius: 20px;
+  background: transparent;
+  outline: none;
+  color: #ff3d3d;
+  cursor: pointer;
+}
+
+.dialog-buttons__confirm-btn {
+  padding: 8px 16px;
+  display: inline-block;
+  border: 1px solid #2196f3;
+  border-radius: 20px;
+  background: transparent;
+  outline: none;
+  color: #2196f3;
+  cursor: pointer;
 }
 </style>
